e87584746b1d08fbbf61adac5b439836655f753c
Aurelio Malheiros <aureliomalheiros@gmail.com>
2022-12-17 15:55:03 -0300

    refact: Remove dashboard - Remove configuration YAML - When necessary create folder dashboard and resource - Don't user resource

--- control-plane/control-plane.sh
+++ control-plane/control-plane.sh
@@ -26,56 +26,7 @@ echo "[TASK 05] Salvando a chave"
 
 sudo kubeadm token create --print-join-command > /home/vagrant/kubernetes.sh
 
-echo "[TASK 06] Deployment da interface gráfica"
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml >/dev/null 2>&1
-
-echo "[TASK 07] Criação dos serviços"
-
-cat >>kubernetes-dashboard-service-np.yaml<<EOF
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard
----
-kind: Service
-apiVersion: v1
-metadata:
-  namespace: kubernetes-dashboard
-  name: kubernetes-dashboard-service-np
-  labels:
-    k8s-app: kubernetes-dashboard
-spec:
-  type: NodePort
-  ports:
-  - port: 8443
-    nodePort: 30002
-    targetPort: 8443
-    protocol: TCP
-  selector:
-    k8s-app: kubernetes-dashboard
-EOF
-
-echo "[TASK 08] Aplicando os serviços"
-
-sudo kubectl apply -f kubernetes-dashboard-service-np.yaml > /dev/null 2>&1
-
-echo "[TASK 09] Armazenando a chave"
+echo "[TASK 06] Armazenando a chave"
 
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') > token-dashboard.log
 
